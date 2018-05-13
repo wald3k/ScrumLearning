574fa9e8543b0c5cbb71feb44de8372709793aa7
Waldemar Sobiecki <waldek.sob@gmail.com>
2018-05-13 22:46:26 +0200

    Removing lines from gitignore. Running initialization script. Commening STATIC_ROOT due to issues with displaying static images, templates etc.

--- ScrumLearning/settings.py
+++ ScrumLearning/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 #These STATICFILES_DIRS are common for all applications!
