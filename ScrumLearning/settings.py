d4cdb5b4a4c2bc2d64d397c1705e690c402d93f0
Waldemar Sobiecki <waldek.sob@gmail.com>
2018-05-13 13:27:10 +0200

    Modifying allowed hosts in settings.py so that application acn be moved on a production server.

--- ScrumLearning/settings.py
+++ ScrumLearning/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'y6*_-7=wtl)qhj3+q=3d&6-f)!ajxrgy_=)t!n=-g(xl-8_v9^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['scrum.sobiecki.me','www.scrum.sobiecki.me','localhost']
 
 
 # Application definition
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 #These STATICFILES_DIRS are common for all applications!
